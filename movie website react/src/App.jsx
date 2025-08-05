be5b2acca01e26bf03c9ff50263abb1e01c80111
Suyog Bhandari <108009756+suyog-bhandari@users.noreply.github.com>
2025-08-05 20:47:41 +0545

    Add files via upload

---
+++ movie website react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MovieCard from './Component/MovieCard'
+import Home from "./assets/pages/Home"
+
+function App() {
+  return (
+    <>
+      <Home/>
+   </>
+  )
+}
+
+export default App
