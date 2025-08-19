205f94f524c6c588a3f1d44b5848fed60202a75c
Suyog Bhandari <108009756+suyog-bhandari@users.noreply.github.com>
2025-08-19 08:36:50 +0545

    Add files via upload

---
+++ React-Index-routing/src/Unknown1.jsx
@@ -0,0 +1,7 @@
+export default function Unknown1(){
+    return(
+        <>
+            <h1>Unknown 1</h1>
+        </>
+    )
+}
---
+++ React-Index-routing/src/Unknown2.jsx
@@ -0,0 +1,7 @@
+export default function Unknown2(){
+    return(
+        <>
+            <h1>Unknown 2</h1>
+        </>
+    )
+}
